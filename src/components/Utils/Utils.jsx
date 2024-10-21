e8a028b0290366a8508511cacc8ae6c2650c41cb
Alejandro <gmz248alejandro@gmail.com>
2024-10-21 17:59:55 -0300

    Se simplificó la creación de días en la función asignarDiasFrancos en el componente Utils

--- src/components/Utils/Utils.jsx
+++ src/components/Utils/Utils.jsx
@@ -4,7 +4,7 @@ export const shuffleArray = (array) =>
     .sort((a, b) => a.sort - b.sort);
 
 export const asignarDiasFrancos = (empleados) => {
-  const dias = Array.from({ length: 7 }, (_, i) => i);
+  const dias = [0, 1, 2, 3, 4, 5, 6];
   const diasAsignados = shuffleArray(dias);
 
   return empleados.map((empleado, index) => ({
